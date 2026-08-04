5637fbfbae52d2931bdf97ad010d4ecfccda76c2
Benjamin <bgouveia15@yahoo.com>
2026-08-04 16:53:25 -0500

    deploy: build 46 (source 271a6ad)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -76,7 +76,7 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2018512010" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "590924588" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   },
   config: {
     // A path on our own origin. Nothing is served from it by design.
